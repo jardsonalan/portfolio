c5d5b64b5377581ec42fc3ce2728f8e9c8a14ca7
jardsonalan <jardsonalan132@gmail.com>
2024-06-26 01:02:31 -0300

    feat: incluindo uma função de scroll para mudar a cor da headerBar após rolagem; melhoramento no código do javascript; mudanças em alguns elementos no html e css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,105 @@
 'use strict';
 
-function menuShow() {
-    let menuMobile = document.querySelector('.menu-mobile');
-    let iconMode = document.querySelector('.icon');
-    let headerBar = document.querySelector('header.light-theme');
+const menuMobile = document.querySelector('.menu-mobile');
+const iconMode = document.querySelector('.icon');
+const headerBar = document.querySelector('header.light-theme');
+const btnIcon = document.querySelector('#btnIcon');
+const header = document.querySelector('header');
 
-    if (menuMobile.classList.contains('open-menu') == true) {
-        if (headerBar.classList.contains('dark-theme') == true) {
+btnIcon.addEventListener('click', (evt) => {
+    if (menuMobile.classList.contains('open-menu')) {
+        if (headerBar.classList.contains('dark-theme')) {
             menuMobile.classList.remove('open-menu');
             iconMode.src = 'assets/img/icons-menu/menu-burger-dark-mode.png';
+            header.style.backgroundColor = 'transparent';
+            header.style.boxShadow = 'none';
         } else {
             menuMobile.classList.remove('open-menu');
             iconMode.src = 'assets/img/icons-menu/menu-burger.png';
+            header.style.backgroundColor = 'transparent';
+            header.style.boxShadow = 'none';
         }
     } else {
-        if (headerBar.classList.contains('dark-theme') == true) {
+        if (headerBar.classList.contains('dark-theme')) {
             menuMobile.classList.add('open-menu');
             iconMode.src = 'assets/img/icons-menu/cross-dark-mode.png';
+            header.style.backgroundColor = 'var(--bgColor)';
+            header.style.boxShadow = '0 3px 10px var(--shadowBar)';
         } else {
             menuMobile.classList.add('open-menu');
             iconMode.src = 'assets/img/icons-menu/cross.png';
+            header.style.backgroundColor = 'var(--bgColor)';
+            header.style.boxShadow = '0 3px 10px var(--shadowBar)';
         }
     }
-}
+})
 
-function setColorPage() {
-    let buttonModeColor = document.querySelector('.color-button');
-    let headerBar = document.querySelector('header.light-theme');
-    let logoMode = document.querySelector('.logo-img');
-    let iconModeColor = document.querySelector('.icon');
-    let iconModeTheme = document.querySelector('.mode-theme');
-    let menuMobile = document.querySelector('.menu-mobile');
-    let imageHome = document.querySelector('.home-light');
+const btnColorMode = document.querySelector('#btnColorMode');
 
-    if (buttonModeColor.classList.contains('dark-theme') == true) {
-        buttonModeColor.classList.remove('dark-theme');
+btnColorMode.addEventListener('click', (evt) => {
+    const logoMode = document.querySelector('.logo-img');
+    const iconModeTheme = document.querySelector('.mode-theme');
+    const imageHome = document.querySelector('.home-light');
+
+    if (btnColorMode.classList.contains('dark-theme')) {
+        btnColorMode.classList.remove('dark-theme');
         document.body.classList.remove('dark-theme');
         headerBar.classList.remove('dark-theme');
         logoMode.src = 'assets/img/logos/logo-light-mode.png';
         iconModeTheme.src = 'assets/img/light-dark-theme/moon.png';
         imageHome.classList.remove('home-dark');
 
-        if (menuMobile.classList.contains('open-menu') == true) {
-            iconModeColor.src = 'assets/img/icons-menu/cross.png';
-        } else {
-            iconModeColor.src = 'assets/img/icons-menu/menu-burger.png';
-        }
-    } else {
-        buttonModeColor.classList.add('dark-theme');
-        document.body.classList.add('dark-theme');
-        headerBar.classList.add('dark-theme');
-        logoMode.src = 'assets/img/logos/logo-dark-mode.png';
-        iconModeColor.src = 'assets/img/icons-menu/menu-burger-dark-mode.png';
-        iconModeTheme.src = 'assets/img/light-dark-theme/brightness.png';
-        imageHome.classList.add('home-dark');
-
-        if (menuMobile.classList.contains('open-menu') == true) {
-            iconModeColor.src = 'assets/img/icons-menu/cross-dark-mode.png';
-        } else {
-            iconModeColor.src = 'assets/img/icons-menu/menu-burger-dark-mode.png';
-        }
-    }
-}
-
-const touchMenu = () => {
-    let menuMobile = document.querySelector('.menu-mobile');
-    let iconMode = document.querySelector('.icon');
-    let headerBar = document.querySelector('header.light-theme');
-
-    if (menuMobile.classList.contains('open-menu') == true) {
-        menuMobile.classList.remove('open-menu');
-
-        if (headerBar.classList.contains('dark-theme') == true) {
-            iconMode.src = 'assets/img/icons-menu/menu-burger-dark-mode.png';
+        if (menuMobile.classList.contains('open-menu')) {
+            iconMode.src = 'assets/img/icons-menu/cross.png';
         } else {
             iconMode.src = 'assets/img/icons-menu/menu-burger.png';
         }
+    } else {
+        btnColorMode.classList.add('dark-theme');
+        document.body.classList.add('dark-theme');
+        headerBar.classList.add('dark-theme');
+        logoMode.src = 'assets/img/logos/logo-dark-mode.png';
+        iconMode.src = 'assets/img/icons-menu/menu-burger-dark-mode.png';
+        iconModeTheme.src = 'assets/img/light-dark-theme/brightness.png';
+        imageHome.classList.add('home-dark');
+
+        if (menuMobile.classList.contains('open-menu')) {
+            iconMode.src = 'assets/img/icons-menu/cross-dark-mode.png';
+        } else {
+            iconMode.src = 'assets/img/icons-menu/menu-burger-dark-mode.png';
+        }
+    }
+})
+
+const touchMenuMobile = [...document.querySelectorAll('.nav-link-mobile')];
+
+touchMenuMobile.map((el) => {
+    el.addEventListener('click', (evt) => {
+        if (menuMobile.classList.contains('open-menu')) {
+            menuMobile.classList.remove('open-menu');
+
+            if (el.textContent.toLocaleUpperCase() == touchMenuMobile[0].textContent.toLocaleUpperCase()) {
+                header.style.backgroundColor = 'transparent';
+                header.style.boxShadow = 'none';
+            }
+    
+            if (headerBar.classList.contains('dark-theme')) {
+                iconMode.src = 'assets/img/icons-menu/menu-burger-dark-mode.png';
+            } else {
+                iconMode.src = 'assets/img/icons-menu/menu-burger.png';
+            }
+        }
+    })
+})
+
+function activeScroll() {
+    if (scrollY > 50 || menuMobile.classList.contains('open-menu')) {
+        header.style.backgroundColor = 'var(--bgColor)';
+        header.style.boxShadow = '0 3px 10px var(--shadowBar)';
+    } else {
+        header.style.backgroundColor = 'transparent';
+        header.style.boxShadow = 'none';
     }
 }
+
+window.addEventListener('scroll', activeScroll);
