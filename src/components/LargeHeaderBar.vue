383031c102ec54e1f75109526bd6f8a1873fe046
Jardson Alan <jardsonalan132@gmail.com>
2024-11-25 00:37:59 -0300

    wip: home view and header bar in creation

---
+++ src/components/LargeHeaderBar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <VNavigationDrawer
+    class="bg-grey-lighten-3"
+    expand-on-hover
+    rail
+  >
+    <VList>
+      <VListItem
+        prepend-avatar="/public/perfil.jpeg"
+        title="Jardson Alan"
+      ></VListItem>
+    </VList>
+
+    <VDivider></VDivider>
+
+    <VList density="compact" nav>
+      <VListItem prepend-icon="mdi-home" title="Home" value="home"></VListItem>
+      <VListItem prepend-icon="mdi-account" title="About" value="about"></VListItem>
+      <VListItem prepend-icon="mdi-list-box" title="Skills" value="skills"></VListItem>
+      <VListItem prepend-icon="mdi-application-brackets" title="Projects" value="projects"></VListItem>
+    </VList>
+  </VNavigationDrawer>
+</template>
+
+<style lang="scss" module>
+
+</style>
---
+++ src/components/SmallHeaderBar.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup></script>
+
+<template>
+  <VNavigationDrawer
+    class="bg-grey-lighten-3"
+    temporary
+  >
+    <VList>
+      <VListItem
+        prepend-avatar="/public/perfil.jpeg"
+        title="Jardson Alan"
+      ></VListItem>
+    </VList>
+
+    <VDivider></VDivider>
+
+    <VList density="compact" nav>
+      <VListItem prepend-icon="mdi-home" title="Home" value="home"></VListItem>
+      <VListItem prepend-icon="mdi-account" title="About" value="about"></VListItem>
+      <VListItem prepend-icon="mdi-list-box" title="Skills" value="skills"></VListItem>
+      <VListItem prepend-icon="mdi-application-brackets" title="Projects" value="projects"></VListItem>
+    </VList>
+  </VNavigationDrawer>
+</template>
+
+<style lang="scss" setup></style>
