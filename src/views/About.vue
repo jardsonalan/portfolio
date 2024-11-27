e0f0641aa26498cc0cf14388bd1c96f5c51e91cd
Jardson Alan <jardsonalan132@gmail.com>
2024-11-26 21:25:17 -0300

    feat: adding the about view screen

---
+++ src/views/About.vue
@@ -0,0 +1,90 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script lang="ts" setup>
+const links = [
+  { id: 1, nome: 'Linkedin', icon: 'mdi-linkedin', href: 'https://www.linkedin.com/in/jardson-alan/', target: '_blank' },
+  { id: 2, nome: 'GitHub', icon: 'mdi-github', href: 'https://github.com/jardsonalan', target: '_blank' },
+  { id: 3, nome: 'YouTube', icon: 'mdi-youtube', href: 'https://www.youtube.com/@JardsonAlan', target: '_blank' },
+  { id: 4, nome: 'Instagram', icon: 'mdi-instagram', href: 'https://www.instagram.com/jardson.alan/', target: '_blank' },
+]
+</script>
+
+<template>
+  <VContainer class="d-flex d-md-none d-lg-none align-center h-screen">
+    <VCard class="mx-auto pa-3" width="300">
+      <VCardTitle class="mb-2 text-center text-h4 font-weight-bold">About <span :id="about.spanColor">me</span></VCardTitle>
+      <VImg
+        class="rounded-circle mx-auto"
+        src="/perfil.jpeg"
+        width="120"
+      ></VImg>
+      <VCardText>
+        <ul class="mt-4 text-center">
+          <li class="mb-2">Graduated in IT Technician</li>
+          <li class="mb-2">Studying Internet Systems</li>
+          <li class="mb-2">Basic to Intermediate English</li>
+        </ul>
+      </VCardText>
+      <VRow class="d-flex mt-2">
+        <VCol
+          v-for="link in links"
+          :key="link.id"
+        >
+          <VBtn
+            :icon="link.icon"
+            :href="link.href"
+            :target="link.target"
+          ></VBtn>
+        </VCol>
+      </VRow>
+    </VCard>
+  </VContainer>
+
+  <VContainer class="d-none d-md-flex d-lg-flex align-center h-screen">
+    <VCard class="d-flex mx-auto pa-3" width="800">
+      <div>
+        <VCardTitle class="mb-2 text-center text-h4 font-weight-bold">About <span :id="about.spanColor">me</span></VCardTitle>
+        <VImg
+          class="rounded-circle mx-auto"
+          src="/perfil.jpeg"
+          width="150"
+        ></VImg>
+        <VCardText>
+          <ul class="mt-4 text-center">
+            <li class="mb-2">Graduated in IT Technician</li>
+            <li class="mb-2">Studying Internet Systems</li>
+            <li class="mb-2">Basic to Intermediate English</li>
+          </ul>
+        </VCardText>
+      </div>
+      <VDivider vertical></VDivider>
+      <VRow class="d-flex flex-column">
+        <VCol
+          class="d-flex flex-column justify-center align-center"
+          v-for="link in links"
+          :key="link.id"
+        >
+          <VBtn
+            width="500"
+            :prepend-icon="link.icon"
+            :href="link.href"
+            :target="link.target"
+          >
+            {{ link.nome }}
+          </VBtn>
+        </VCol>
+      </VRow>
+    </VCard>
+  </VContainer>
+</template>
+
+<style lang="scss" module="about">
+@use "../sass/settings.scss" as settings;
+
+#spanColor {
+  @include settings.spanColor;
+}
+
+li {
+  list-style: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,25 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+</script>
 
 <template>
   <VParallax src="/background-light.jpg">
     <div class="d-flex flex-column justify-center align-center h-screen">
       <div class="mb-4">
-        <span :class="home.font" class="text-subtitle-2 text-md-subtitle-1 text-lg-subtitle-1 font-weight-bold mr-4 px-4 py-1 rounded-lg bg-blue">Hello</span>
-        <span :class="home.font" class="text-subtitle-2 text-md-subtitle-1 text-lg-subtitle-1 font-weight-medium">I am</span>
+        <span class="text-subtitle-2 text-md-subtitle-1 text-lg-subtitle-1 font-weight-bold mr-4 px-4 py-1 rounded-lg bg-blue">Hello</span>
+        <span class="text-subtitle-2 text-md-subtitle-1 text-lg-subtitle-1 font-weight-medium">I am</span>
       </div>
-      <h1 :class="home.font" class="text-h4 text-md-h1 text-lg-h1 mb-4 font-weight-bold">Jardson Alan</h1>
-      <h4 :class="home.font" class="text-center text-subtitle-2 text-md-subtitle-1 text-lg-subtitle-1 font-weight-bold"><span :id="home.spanColor">Front-end</span> Developer</h4>
+      <h1 class="text-h4 text-md-h1 text-lg-h1 mb-4 font-weight-bold">Jardson Alan</h1>
+      <h4 class="text-center text-subtitle-2 text-md-subtitle-1 text-lg-subtitle-1 font-weight-bold"><span :id="home.spanColor">Front-end</span> Developer</h4>
     </div>
   </VParallax>
 </template>
 
 <style lang="scss" module="home">
-@import '../sass/settings.scss';
-
-.font {
-  font-family: $font-h1;
-}
+@use "../sass/settings.scss" as settings;
 
 #spanColor {
-  color: $span-color;
+  @include settings.spanColor;
 }
 </style>
