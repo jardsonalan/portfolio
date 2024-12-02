9f0cc98dec199b152d66115ae110795150d854ea
Jardson Alan <jardsonalan132@gmail.com>
2024-12-02 17:36:27 -0300

    style: adjustment in responsiveness

--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,14 @@ provide('isDrawer', isDrawer)
   <VApp>
     <VCard flat>
       <VLayout>
-        <LargeHeaderBar class="d-none d-md-inline d-lg-inline"/>
-        <SmallHeaderBar class="d-inline d-md-none d-lg-none" v-model="isDrawer"/>
+        <LargeHeaderBar class="d-none d-lg-inline"/>
+        <SmallHeaderBar class="d-inline d-lg-none" v-model="isDrawer"/>
         <VMain>
           <RouterView />
         </VMain>
       </VLayout>
     </VCard>
-    <VBtn class="d-sx-inline d-md-none d-lg-none position-fixed right-0 ma-2" style="width: 30px; height: 30px;" @click.stop="isDrawer = !isDrawer" icon="mdi-menu"></VBtn>
+    <VBtn class="d-inline d-lg-none position-fixed right-0 ma-2" style="width: 30px; height: 30px;" @click.stop="isDrawer = !isDrawer" icon="mdi-menu"></VBtn>
   </VApp>
 </template>
 
